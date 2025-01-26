14dea1a2da00778d9bd9d2c66ebded2e5dace4c6
AakashKainth <aakash.kainth@gmail.com>
2025-01-26 08:39:07 -0500

    Add project files.

---
+++ StudentApi/Data/SchoolContext.cs
@@ -0,0 +1,14 @@
+﻿// Data/SchoolContext.cs
+using Microsoft.EntityFrameworkCore;
+using StudentApi.Models;
+using System.Collections.Generic;
+
+namespace StudentApi.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options) { }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ StudentApi/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StudentApi.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        [Column("Student_Name", TypeName = "varchar(100)")]
+        public string FirstName { get; set; }
+        [Column("Student_LastName", TypeName = "varchar(100)")]
+        public string LastName { get; set; }
+        [Column("Student_Email", TypeName = "varchar(100)")]
+        public string Email { get; set; }
+    }
+}
